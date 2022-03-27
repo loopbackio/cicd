7520ce38bc5c229320e72a8a9aaa30f13f5c246b
Rifa Achrinza <25147899+achrinza@users.noreply.github.com>
2022-03-28 00:07:55 +0800

    fix: base renovate config
    Signed-off-by: Rifa Achrinza <25147899+achrinza@users.noreply.github.com>

--- scripts/lint-renovate.sh
+++ scripts/lint-renovate.sh
@@ -7,7 +7,7 @@ export POSIXLY_CORRECT=1
 SCRIPT_DIR="$(dirname $0)"
 RENOVATE_CONFIG_ROOT="$SCRIPT_DIR/../shared-configs/renovate"
 
-find "$RENOVATE_CONFIG_ROOT" -name *.yaml -type f | \
+find "$RENOVATE_CONFIG_ROOT" -name '*.json' -type f | \
     xargs -I'{}' \
         env RENOVATE_CONFIG_FILE='{}' \
             npm exec \
